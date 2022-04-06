030d667e967d1613f0984b57c8968241e5b90fca
Ubuntu <ubuntu@ip-172-31-36-188.us-west-2.compute.internal>
2022-04-06 23:56:39 +0000

    html refactoring

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  # テストが実行される前に実行されるメソッド
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   # home pageに入ったとき、レスポンスはsuccessになるはず
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    # 特定のhtmlタグがあるかどうかチェック
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
