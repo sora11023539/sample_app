b6ed6261c2a6288a015b1f80b50745a931ae493d
Ubuntu <ubuntu@ip-172-31-36-188.us-west-2.compute.internal>
2022-04-14 21:54:40 +0000

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
   
   # ページごとの完全なタイトルを返す
   def full_title(page_title = '')
-    base_title = 'Ruby on Rails Tutorial Sample App"'
+    base_title = 'Ruby on Rails Tutorial Sample App'
     # ページタイトルがからの場合
     if page_title.empty?
       base_title
     else
-      page_title + " |  " + base_title
+      page_title + " | " + base_title
     end
   end
 end
