ee1ec72948465840808d6c8b2e7924d344f4ff64
Ubuntu <ubuntu@ip-172-31-36-188.us-west-2.compute.internal>
2022-04-27 14:12:22 +0000

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
-  def new
+  
+  # showアクション
+  def show
+    # @user変数定義
+    # find dbからユーザーを取り出
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    # 保存されたら
+    if @user.save
+      # flash 1回のみ succsessにメッセージ
+      flash[:success] = "Welcome to the Sample App!"
+      # redirect_to user_url(@user)と等価
+      # ユーザー情報ページにリダイレクト
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  # 外部から使えないように
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  # ユーザー情報を表示するURLを追加する
+  # resour 7つのアクションを自動で定義
+  resources :users
 end
